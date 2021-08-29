1733b99933715b279bb85a75284e16b4a1ef2127
AlonSilbe <88971240+AlonSilbe@users.noreply.github.com>
2021-08-29 12:00:54 +0300

    Update Todo_SQL_Python.py

--- Todo_SQL_Python.py
+++ Todo_SQL_Python.py
@@ -26,19 +26,35 @@ def add():
 #creates the file for the list and a variable resmebling the file in the code
     conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
 #Creates the table for the todo list in the file
-    conn.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY ,category char(50), theitem char(100))")
+    conn.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY ,category char(50), item char(100))")
 #insert users inputs into table
     while i<=numz:
         category=input('Please enter what category of what you want to do')
         item=input('pleses enter the item you want to buy or the activite you want to do')
-        conn.execute("INSERT INTO todo (category, theitem) VALUES ('{}','{}')".format(category,item))
+        conn.execute("INSERT INTO todo (category, item) VALUES ('{}','{}')".format(category,item))
         i=i+1
     conn.commit()
+    menu()
+
+def select_col(conn):
+    cur = conn.cursor()
+    col=input("insert column name you want to fetch (id, category, item)")
+    if col=="item" or col=="id" or col=="category" :
+        cur.execute("SELECT {} FROM todo".format(col))
+    else:  
+        print('invalid input')
+        select_col(conn)
+#Create a list containing all the values in the database
+    rows = cur.fetchall()
+#print each row
+    for row in rows:
+        print(row)
     menu()
 
 def dele():
         conn = sqlite3.connect('todo.db')
         conn.execute("DELETE FROM todo")
+        conn.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY ,category char(50), item char(100))")
         conn.commit()
         print('All values have been deleted')
         menu()
@@ -50,24 +66,25 @@ def menu():
     
     #creates the file for the list and a variable resmebling the file in the code
     conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
+    conn.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY ,category char(50), item char(100))")
     choice = input("""
                       A: Add assignments to the todo list
-                      B: deltete all values in the table
-                      C: print the DB
+                      B: delte all values in the table
+                      C: print the entire DB
+                      D:print single column
                     
-
                       Please enter your choice: """)
-
+   
     if choice == "A" or choice =="a":
         add()
     elif choice == "B" or choice =="b":
         dele()
     elif choice == "C" or choice =="c":
         select_all(conn)
-    #elif choice=="Q" or choice=="q":
-     #   sys.exit
+    elif choice == "D" or choice =="d":
+        select_col(conn)
     else:
-        print("You must only select either A or B")
+        print("You must only select either A B C or D")
         print("Please try again")
         menu()
         
